3026b45284a24f0e2aa581de0ff4aced146bdeaa
Christian Raymond <christianfraymond@gmail.com>
2025-08-07 22:06:20 +1000

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -13,14 +13,12 @@ export const experienceData: Experience[] = [
     date: "Jun. 2025 — Present",
     title: "Senior Applied Scientist",
     company: "Oracle",
-    description: "Scientist at Oracle Health and AI (OHAI) utilizing large language models and generative AI techniques to develop applications that support and streamline healthcare. ",
-    // manager: "Dr Peter Zhong"
+    description: "Scientist at Oracle Health and AI (OHAI) working on natural language processing and reinforcement learning, developing models that support and streamline healthcare.",
   },
   {
     date: "Sep. 2024 — Mar. 2025",
     title: "Applied Scientist",
     company: "Amazon",
     description:"Internship in the AU computer vision team, automating the labor-intensive process of creating 3D assets for Amazon’s storefront by designing and implementing a foundation model for 3D geometry and material generation.",
-    // advisor: "Dr Violetta Shevchenko"
   },
 ];
